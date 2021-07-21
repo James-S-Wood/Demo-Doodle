96a930edceb4fae7cf50a16c86c83d276c497d9a
jameswood <james_s_wood@outlook.com>
2021-07-21 12:39:06 -0400

    Adding files in project 'Demo-Doodle' to Git...

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo_Doodle
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
